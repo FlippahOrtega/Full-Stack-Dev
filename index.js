a01f3541590f69ad4ca2162ca0e3ed07dc2b9e02
Felipe Ortega <felipeortega@MacBook-Pro-de-Felipe.local>
2021-05-23 11:24:56 -0500

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+require('dotenv').config()
+
+const app = express();
+
+// capturar body
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Conexión a Base de datos
+
+// import routes
+const authRoutes = require('./routes/auth');
+// route middlewares
+app.use('/api/user', authRoutes);
+
+app.get('/', (req, res) => {
+    res.json({
+        estado: true,
+        mensaje: 'funciona!'
+    })
+});
+
+// iniciar server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`servidor andando en: ${PORT}`)
+})
