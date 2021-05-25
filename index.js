7203d9d1ab62614da5d88b758ba5be547a11a4c3
flippah <flippah@live.com>
2021-05-25 11:42:30 -0500

    User SignUp

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Conexión a Base de datos
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@clusterstack.hloek.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+mongoose.connect(uri,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+.then(() => console.log('Base de datos conectada'))
+.catch(e => console.log('error db:', e))
 
 // import routes
 const authRoutes = require('./routes/auth');
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,56 @@
 const router = require('express').Router();
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+// password
+const bcrypt = require('bcrypt');
+
+// validation
+const Joi = require('@hapi/joi');
+
+const schemaRegister = Joi.object({
+    name: Joi.string().min(6).max(255).required(),
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+})
+
+const schemaLogin = Joi.object({
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+})
 
 router.post('/register', async (req, res) => {
 
-    res.json({
-        error: null,
-        data: 'aquí va ir la data'
-    })
+    // validate user
+    const { error } = schemaRegister.validate(req.body)
+    
+    if (error) {
+        return res.status(400).json({error: error.details[0].message})
+    }
+
+    const isEmailExist = await User.findOne({ email: req.body.email });
+    if (isEmailExist) {
+        return res.status(400).json({error: 'Email already registered'})
+    }
+
+    // hash contraseña
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, salt);
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: password
+    });
+    try {
+        const savedUser = await user.save();
+        res.json({
+            error: null,
+            data: savedUser
+        })
+    } catch (error) {
+        res.status(400).json({error})
+    }
 })
 
 module.exports = router;
